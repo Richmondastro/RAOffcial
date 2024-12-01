0180bf35b73a613d61087ba6bf6bad42cc426edb
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-12-01 13:43:41 +0530

    link the registration form

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 2, 2024 00:00:00").getTime();
+var countDownDate = new Date("Dec 1, 2024 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
@@ -24,6 +24,6 @@ var x = setInterval(function () {
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("countdownTimer").innerHTML =
-      "<span class='register-text bg-info'>First Round Registrations</span><br /><br /><a href='#' class='primary-btn'>Register your Team</a>";
+      "<span class='register-text bg-info'>First Round Registrations</span><br /><br /><a href='./registration.html' class='primary-btn'>Register your Team</a>";
   }
 }, 1000);

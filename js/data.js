59f2c3d8ea37029bbf66b1a2dfb0a995abe89c10
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-12-11 05:52:09 +0530

    update results page into overview and update overview page with data

---
+++ js/data.js
@@ -0,0 +1,16 @@
+const schoolCodes = [
+  {
+    SchoolName: "Richmond College, Galle",
+    SchoolCode: "xxx-xxx-xx",
+    Team: "A",
+  },
+];
+
+const results = [
+  {
+    Place: "1",
+    SchoolName: "Richmond College, Galle",
+    TeamCode: "xxx-xxx-xx",
+    Marks: 100,
+  },
+];
